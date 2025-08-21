5bf2f03929c763d13e847b7d86ebbe45289f66f2
Giovanni-A-Gamis <alcaraz.gamis.g@gmail.com>
2025-08-20 23:52:02 -0300

    feat:molde do login

--- src/navigation/screens/Login/index.js
+++ src/navigation/screens/Login/index.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import { styles } from './styles';
+import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from "@expo/vector-icons";
 
 export default function LoginScreen({ navigation }) {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [showPassword, setShowPassword] = React.useState(false);
+    
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Login Screen</Text>
+            <Text style={styles.title}>Seja bem vindo de volta!</Text>
+            <StatusBar style="auto" />
+        
+            <View style={styles.allinput}>
+                <View style={styles.inputContainer}>
+                <TextInput 
+                    style={styles.input} 
+                    placeholder="Email" 
+                    value={email} 
+                    onChangeText={setEmail} 
+                    keyboardType="email-address"
+                />
+            </View>
+            
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Senha"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity 
+                    onPress={() => setShowPassword(!showPassword)}
+                    style={styles.iconContainer}
+                >
+                    <Ionicons
+                        name={showPassword ? "eye-off" : "eye"}
+                        size={24}
+                        color="gray"
+                    />
+                </TouchableOpacity>
+            </View>
+
+            </View>
+
+            
             <TouchableOpacity 
                 style={styles.button}
                 onPress={() => navigation.navigate('MainContainer')}
--- src/navigation/screens/Login/styles.js
+++ src/navigation/screens/Login/styles.js
@@ -7,4 +7,46 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-})
+    title: {
+        fontSize: 24,
+        fontFamily: 'Poppins_700Bold',
+    },
+    button: {
+        backgroundColor: '#17285D',
+        borderRadius: 8,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 16,
+        paddingVertical: 14,
+        paddingHorizontal: 40,
+        textAlign: 'center',
+        fontFamily: 'Poppins_400Regular',
+    },
+    inputContainer: {
+        flexDirection: 'row', 
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        width: '80%',
+        height: 45,
+        marginVertical: 10,
+        paddingHorizontal: 10,
+    },
+    input: {
+        flex: 1,
+        fontFamily: 'Poppins_400Regular',
+        fontSize: 16,
+    },
+    iconContainer: {
+        marginLeft: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    allinput: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+    },
+});
