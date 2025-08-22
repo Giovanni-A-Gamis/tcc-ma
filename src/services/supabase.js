6bebee9872804ceb83a8be176a26e563452d39c0
GIOVANNI ALCARAZ GAMIS <giovanni.gamis@etec.sp.gov.br>
2025-08-22 17:26:43 -0300

    update:seguranca dos dados do supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
 const supabase = axios.create({
-    baseUrl: 'https://onakobymklswoaqexsct.supabase.co/rest/v1/',
+    baseURL: `${SUPABASE_URL}/rest/v1/`,
     headers: {
-        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uYWtvYnlta2xzd29hcWV4c2N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3NDc1NTEsImV4cCI6MjA3MTMyMzU1MX0.0fQFDv4Viei5g8R9-tl0rh9-b0tbZfI8v8O9sgRTI80'
-    }
-})
+        apikey: SUPABASE_ANON_KEY,
+        Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+    },
+});
 
-export default supabase
+export default supabase;
